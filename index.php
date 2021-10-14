349af852fe54110b1325be3123bf0074ee79f000
ssrly <st.reinhold@vodafonemail.de>
2021-10-14 08:24:14 +0200

    add lib

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../../lib/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <script src="./../../lib/jquery/jquery-3.6.0.min.js"></script>
+    <script src="./../../lib/jquery-ui/jquery-ui.min.js"></script>
+    <script src="./../../lib/jquery-validation/dist/jquery.validate.min.js"></script>
+    <script src="./js/script.js"></script>
     <title>Tempus Fugit</title>
 </head>
 
--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+jQuery(document).ready(function() {
+
+    console.log('hello world!');
+
+});
