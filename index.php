aed3e28049b17924aa9c0d0f066466b641643aa3
ssrly <st.reinhold@vodafonemail.de>
2021-10-14 08:33:20 +0200

    add favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="./assets/img/favicon.ico">
     <link rel="stylesheet" href="./../../lib/fontawesome/css/all.min.css" />
     <link rel="stylesheet" href="./css/style.css" />
     <script src="./../../lib/jquery/jquery-3.6.0.min.js"></script>
