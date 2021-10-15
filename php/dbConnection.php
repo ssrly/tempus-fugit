4357df380feff0071ef96e60811ef57408890a08
ssrly <st.reinhold@vodafonemail.de>
2021-10-15 16:37:42 +0200

    remove comment

--- php/dbConnection.php
+++ php/dbConnection.php
@@ -6,15 +6,3 @@ $options = [
 $db = new PDO('mysql:host=localhost;dbname=tempus_fugit','root','',$options);
 
 $db->query('SET NAMES utf8');
-
-
-#TODO: remove
-// $sql = 'SELECT * FROM times INNER JOIN users
-//           ON times.user_id = users.id';
-
-// $statement = $db->query($sql);
-// $daten = $statement->fetchAll();
-// $count = $statement->rowCount();
-
-// debugIterations($daten);
-// debug($count);
