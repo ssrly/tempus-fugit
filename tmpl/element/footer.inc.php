b0fdcccb047f7eec0d6f09ea1eb889a563a62934
ssrly <st.reinhold@vodafonemail.de>
2021-10-15 10:42:22 +0200

    add basic stylings

--- tmpl/element/footer.inc.php
+++ tmpl/element/footer.inc.php
@@ -1,3 +1,10 @@
 <footer>
-    Footer
+    <a href="index.php?page=impressum" class="copyright">
+        <div class="copyright-logo">
+            <i class="far fa-copyright"></i>
+        </div>
+        <div class="copyright-name">
+            <p>St. Reinhold - 10/2021</p>
+        </div>
+    </a>
 </footer>
--- tmpl/element/header.inc.php
+++ tmpl/element/header.inc.php
@@ -1,10 +1,10 @@
 <header>
-    <div class="brand">
+    <a href="index.php?page=home" class="brand">
         <div class="brand-logo">
             <i class="fas fa-stopwatch"></i>
         </div>
         <div class="brand-name">
             <h1>Tempus Fugit</h1>
         </div>
-    </div>
+    </a>
 </header>
