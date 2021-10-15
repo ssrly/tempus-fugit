b39b3924739c596fd1b13788590ac8332c9f4faa
ssrly <st.reinhold@vodafonemail.de>
2021-10-15 16:17:02 +0200

    add login form

---
+++ tmpl/form/login.inc.php
@@ -0,0 +1,18 @@
+<form id="login-form" action="#" method="POST">
+    <div class="form-group">
+        <input id="login-form-email" class="form-text-input required" type="mail" name="email" placeholder="Your E-Mail"
+            required>
+    </div>
+    <div class="form-group">
+        <input id="login-form-pwd" class="form-text-input required" type="password" name="pwd"
+            placeholder="Your Password" required>
+    </div>
+    <div class="form-group">
+        <p id="form-required-info">Required</p>
+    </div>
+    <div class="form-group">
+        <input id="login-form-register" class="btn btn-default" type="submit" name="Register" value="Register">
+        <input id="login-form-reset" class="btn-danger" type="reset" name="Reset" value="Reset">
+        <input id="login-form-login" class="btn btn-success" type="submit" name="Login" value="Login">
+    </div>
+</form>
