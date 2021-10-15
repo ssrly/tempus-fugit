7af0c34f5fd7c040eaa017a041f4be9dfe01a260
ssrly <st.reinhold@vodafonemail.de>
2021-10-15 09:28:40 +0200

    add brand

--- tmpl/element/header.inc.php
+++ tmpl/element/header.inc.php
@@ -1,3 +1,10 @@
 <header>
-    <h1>Header - Tempus Fugit</h1>
+    <div class="brand">
+        <div class="brand-logo">
+            <i class="fas fa-stopwatch"></i>
+        </div>
+        <div class="brand-name">
+            <h1>Tempus Fugit</h1>
+        </div>
+    </div>
 </header>
