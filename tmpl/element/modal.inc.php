ca83c133313a8831e78944ff90b43e4877eeb166
ssrly <st.reinhold@vodafonemail.de>
2021-10-15 23:45:03 +0200

    add form by page

--- tmpl/element/modal.inc.php
+++ tmpl/element/modal.inc.php
@@ -1,7 +1,15 @@
 <div class="modal">
     <div class="modal-content">
-        <?php // include_once './tmpl/form/login.inc.php' ?>
-        <?php // include_once './tmpl/form/time.inc.php' ?>
-        <?php // include_once './tmpl/form/user.inc.php' ?>
+        <?php
+        /** @var $page ./../../index.php */
+        if ($page === 'users' || $page === 'settings') {
+            include_once './tmpl/form/user.inc.php';
+        } elseif ($page === 'times') {
+            include_once './tmpl/form/time.inc.php';
+        } elseif ($page === 'groups') {
+            include_once './tmpl/form/group.inc.php';
+        } else {
+            include_once './tmpl/form/login.inc.php';
+        } ?>
     </div>
 </div>
