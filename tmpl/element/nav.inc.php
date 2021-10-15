65547e1c6ef68c789a38d81132f6656a05b5b289
ssrly <st.reinhold@vodafonemail.de>
2021-10-15 11:24:08 +0200

    fix class active

--- tmpl/element/nav.inc.php
+++ tmpl/element/nav.inc.php
@@ -1,21 +1,21 @@
 <nav>
     <ul>
-        <li class="<?= empty($page) ? 'active' : ''; ?>">
+        <li class="<?= (empty($page) || $page === 'home') ? 'active' : ''; ?>">
             <a href="index.php?page=home">Home</a>
         </li>
-        <li class="<?= empty($page) ? 'active' : ''; ?>">
+        <li class="<?=  $page === 'users' ? 'active' : ''; ?>">
             <a href="index.php?page=users">Users</a>
         </li>
-        <li class="<?= empty($page) ? 'active' : ''; ?>">
+        <li class="<?=  $page === 'times' ? 'active' : ''; ?>">
             <a href="index.php?page=times">Times</a>
         </li>
-        <li class="<?= empty($page) ? 'active' : ''; ?>">
+        <li class="<?=  $page === 'roles' ? 'active' : ''; ?>">
             <a href="index.php?page=roles">Roles</a>
         </li>
-        <li class="<?= empty($page) ? 'active' : ''; ?>">
+        <li class="<?=  $page === 'docu' ? 'active' : ''; ?>">
             <a href="index.php?page=docu">Dokumentation</a>
         </li>
-        <li class="<?= empty($page) ? 'active' : ''; ?>">
+        <li class="<?=  $page === 'impressum' ? 'active' : ''; ?>">
             <a href="index.php?page=impressum">Impressum</a>
         </li>
     </ul>
