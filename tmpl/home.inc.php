62d60d40581e846c870ab66bb20e225df92ab7ec
ssrly <st.reinhold@vodafonemail.de>
2021-10-25 16:36:55 +0200

    fixed typo

--- tmpl/home.inc.php
+++ tmpl/home.inc.php
@@ -5,11 +5,12 @@
         <strong>If I had more time, I would have written a shorter letter.</strong>
     </p>
     <p class="text-right"><i>- Marcus Tullius Cicero, philosopher and statesman.</i></p>
-    <p>The lack of a automated testing, framework, ORM or even OOP did challenge me quite a lot, nevertheless... its
+    <p>The lack of automated testing, a framework, ORM or even OOP did challenge me quite a lot, nevertheless... its
         done!</p>
     <p>I'm happy, the planed core features could find their way into existence and so the project "Tempus Fugit" is
         usable.</p>
-    <p><a href="https://trello.com/b/zK2v7haI/board" target="_blank">This is my Trello Board</a>, if You are registered, i can give
+    <p><a href="https://trello.com/b/zK2v7haI/board" target="_blank">This is my Trello Board</a>, if You are registered,
+        i can give
         access. It shows the progress of this project pretty accurate.</p>
     <p><a href="https://github.com/ssrly/tempus-fugit" target="_blank">And the GitHub Repository</a>. It is public.</p>
     <h3>Core Features:</h3>
